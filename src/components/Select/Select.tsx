6b55fd97b08df1517a13f009e775b1a6640e73b2
caarlosdamian <dapecarlos@gmail.com>
2022-11-25 20:45:04 -0600

    feat(components): added new componentes select and text input

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import './Select.scss'
+import React, { useState } from "react";
+import arrow from "../../assets/arrow.svg";
+import "./Select.scss";
 
 export const Select = () => {
+  const [showOptions, setShowOptions] = useState(false);
   return (
-    <div>Select</div>
-  )
-}
+    <div
+      className="select-container"
+      onClick={() => setShowOptions(!showOptions)}
+    >
+      <span>Filter by Region</span>
+      <img src={arrow} alt="arrow" />
+      {showOptions && (
+        <ul className="option-container">
+          <li>Africa</li>
+          <li>Americas</li>
+          <li>Asia</li>
+          <li>Europe</li>
+          <li>Oceania</li>
+        </ul>
+      )}
+    </div>
+  );
+};
--- src/sections/controls/Controls.tsx
+++ src/sections/controls/Controls.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import { TextInput } from '../../components/textInput/TextInput'
-import './Controls.scss'
+import React from "react";
+import { Select } from "../../components/select/Select";
+import { TextInput } from "../../components/textInput/TextInput";
+import "./Controls.scss";
 
 export const Controls = () => {
   return (
-    <div className='controls-container'><TextInput/></div>
-  )
-}
+    <div className="controls-container">
+      <TextInput />
+      <Select />
+    </div>
+  );
+};
